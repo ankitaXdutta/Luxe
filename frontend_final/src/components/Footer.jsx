61b2d1508a14d34b56854604e4a1dc9a011d8a9e
Ankita <ankitaxdutta@gmail.com>
2025-03-06 16:31:27 +0530

    Added frontend_final folder

---
+++ frontend_final/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img className="mb-5 w-32" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            © 2025 Luxe of Max | Elevate Your Style with Luxury & Elegance. Shop
+            the finest collections with premium quality and timeless designs
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 1234567890</li>
+            <li>Contact@luxeforeveryou.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ luxe.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
